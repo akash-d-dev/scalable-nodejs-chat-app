6321e38f0aeeac82c0ccdf72c414d629e368e970
akash-d-dev <ak45hhere@gmail.com>
2024-10-03 10:18:21 +0530

    add links to components

--- client/src/components/base/Navbar.tsx
+++ client/src/components/base/Navbar.tsx
@@ -7,7 +7,9 @@ import { CustomUser } from "@/app/api/auth/[...nextauth]/options";
 export default function Navbar({ user }: { user?: CustomUser }) {
   return (
     <nav className='p-6 flex justify-between items-center bg-white shadow-sm'>
-      <h1 className='text-xl md:text-2xl font-extrabold'>ChatBappa</h1>
+      <Link href='/'>
+        <h1 className='text-xl md:text-2xl font-extrabold'>ChatBappa</h1>
+      </Link>
       <div className='flex items-center space-x-2 md:space-x-6 text-gray-700'>
         <Link href='/'>Home</Link>
         <Link href='#features'>Features</Link>
--- client/src/components/groupChat/GroupChatCard.tsx
+++ client/src/components/groupChat/GroupChatCard.tsx
@@ -1,7 +1,9 @@
+"use client";
 import React from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { CustomUser } from "@/app/api/auth/[...nextauth]/options";
 import GroupChatCardMenu from "./GroupChatCardMenu";
+import Link from "next/link";
 
 export default function GroupChatCard({
   group,
@@ -10,18 +12,24 @@ export default function GroupChatCard({
   group: ChatGroupType;
   user: CustomUser;
 }) {
+  console.log("The group", group);
   return (
-    <Card>
-      <CardHeader className='flex-row justify-between items-center '>
-        <CardTitle className='text-2xl'>{group.title}</CardTitle>
-        <GroupChatCardMenu user={user} group={group} />
-      </CardHeader>
-      <CardContent>
-        <p>
-          Passcode :-<strong>{group.passcode}</strong>
-        </p>
-        <p>Created At :-{new Date(group.created_at).toDateString()}</p>
-      </CardContent>
-    </Card>
+    <Link href={`/chat/${group.id}`} target='_blank' rel='noreferrer'>
+      <Card>
+        <CardHeader className='flex-row justify-between items-center '>
+          <CardTitle className='text-2xl'>{group.title}</CardTitle>
+          {/* Stop the click event propagation on the GroupChatCardMenu */}
+          <div onClick={(e) => e.stopPropagation()}>
+            <GroupChatCardMenu user={user} group={group} />
+          </div>
+        </CardHeader>
+        <CardContent>
+          <p>
+            Passcode :-<strong>{group.passcode}</strong>
+          </p>
+          <p>Created At :- {new Date(group.created_at).toDateString()}</p>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
